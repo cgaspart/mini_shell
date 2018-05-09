fe618eeb4bacb12da7f4d0d47d00bd5d08563a48
Corentin GASPART <cgaspart@e1r13p9.42.fr>
2018-05-09 04:58:12 +0200

    Env Modif

--- src/env.c
+++ src/env.c
@@ -12,7 +12,7 @@
 
 #include "minishell.h"
 
-char	**cp_env(void)
+char			**cp_env(void)
 {
 	extern char		**environ;
 	char			**env;
@@ -29,16 +29,36 @@ char	**cp_env(void)
 	return (env);
 }
 
-void	lst_env(char **env, t_env *my_env)
+static char		*get_var_name(char *str);
+{
+	char	*name;
+	int		i;
+
+	i = 0;
+	while (str[i] != ft_strchr(str, '='))
+		i++;
+	name = malloc(sizeof(char) * i + 1);
+	i = 0;
+	while (str[i] != ft_strchr(str, '='))
+	{
+		name[i] = str[i]
+		i++;
+	}
+	name[i] = '\0';
+	return (name);
+}
+
+void			lst_env(char **env, t_env *my_env)
 {
 	while(env[i])
 	{
-		my_env->name = get_var_name(env);
+		my_env->name = get_var_name(env[i]);
+		my_env->data = get_var_data(emv[i]);
 		i++;
 	}
 }
 
-t_env	*get_env(void)
+t_env			*get_env(void)
 {
 	t_env			*my_env;
 	char			**env;
